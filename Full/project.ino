919fe7690dd5d8ade58f943592ed7dc040de7353
Abssdghi <abbas.sadeghi1384@gmail.com>
2025-09-21 14:28:58 +0330

    add(full): merge ESP8266_WIFI and MAX30100 code

--- Full/project.ino
+++ Full/project.ino
@@ -1,23 +1,55 @@
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
 
 const char* ssid     = "ssid";
 const char* password = "password";
 
+long lastBeat = 0;
+float beatsPerMinute;   
+int beatAvg;
+
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  delay(100);
   
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
-
+  Serial.print("Connecting...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
   }
 
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
+  {
+    Serial.println("MAX30105 not found.");
+    while (1);
+  }
+
+  particleSensor.setup();
 
 }
 
-void loop() { }
+void loop() {
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true) {
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      beatAvg = (beatAvg * 0.9) + (beatsPerMinute * 0.1);
+    }
+
+    Serial.print("BPM: ");
+    Serial.println(beatAvg);
+  }
+
+}
